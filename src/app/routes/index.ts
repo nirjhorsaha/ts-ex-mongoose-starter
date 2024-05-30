f0ec0b0820c029adef826e44f076a82c8415a5de
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-05-30 09:51:53 +0600

    added routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+
+// const moduleRoutes = [
+//   {
+//     path: '/test',
+//     route: testRoute,
+//   },
+// ];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
