980a3c1fec3bde167f363d8e8d4d74d5cdc3f974
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-05-30 09:59:46 +0600

    modified app router

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, {Request, Response } from 'express';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+import cors from "cors";
+
 
 const app = express();
 
