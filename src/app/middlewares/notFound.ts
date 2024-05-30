8cd7d40db55263fe5b80733a69a29c5247e5886c
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-05-30 10:04:29 +0600

    middlewares modified

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
@@ -6,7 +7,7 @@ const notFound = (req: Request, res: Response, next: NextFunction) => {
   return res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'API Not Found.!!',
-    error: '',
+    error: error,
   });
 };
 
